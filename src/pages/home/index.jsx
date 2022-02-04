0dfffdecbca72a25458bfe7bc5c302cc68dfc942
ThiagoSilvaMorato <thiagosmorato@gmail.com>
2022-02-04 17:15:35 -0300

    Começando a colocar funcionalidade nos modais e ligando um ao outro

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { UserList } from "../../components/userList";
 import { ModalPayment } from "../../components/modalLayout";
 import { cards } from "../../data/cards";
+import { currencyMask } from "../../utils/currencyMask";
 
 export function Home(props) {
   //Estados
   const [users, setUsers] = useState([]);
   const [isCardModalOpen, setIsCardsModalOpen] = useState(false);
+  const [paymentForUser, setPaymentForUser] = useState(null);
 
   //Coletar dados dos usuários da API
   async function getUsers() {
@@ -20,9 +22,22 @@ export function Home(props) {
   //Hook de efeitos colaterais
   useEffect(getUsers, []);
 
-  function handleButtonOnClick() {
+  function handleButtonOnClick(user) {
+    setPaymentForUser(user);
     setIsCardsModalOpen(true);
   }
+
+  //Validação do valor
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const value = formData.get("valueToPay");
+    const card = formData.get("cardOptions");
+
+    console.log(value);
+    console.log(card);
+  };
 
   return (
     <>
@@ -30,19 +45,28 @@ export function Home(props) {
         //Modal de pagamento
         isCardModalOpen && (
           <ModalPayment
-            firstSpan='Pagamento para'
-            secondSpan=' Nome do Usuário'
+            firstSpan='Pagamento para '
+            secondSpan={paymentForUser.name}
             closeModal={() => setIsCardsModalOpen(false)}>
-            <input type='text' placeholder='R$ 0.00' />
-            <select name='cardOptions'>
-              {cards.map((card) => {
-                console.log(card);
-                return <option value='cards'>{card.card_number}</option>;
-              })}
-            </select>
-            <button type='submit' className='modal__payButton'>
-              Pagar
-            </button>
+            <form onSubmit={handleOnSubmit}>
+              <input
+                name='valueToPay'
+                type='text'
+                placeholder='R$ 0.00'
+                className='inputValue'
+                required
+                onKeyUp={currencyMask}
+              />
+              <select name='cardOptions'>
+                {cards.map((card) => {
+                  console.log(card);
+                  return <option value={card.card_number}>{card.card_number}</option>;
+                })}
+              </select>
+              <button type='submit' className='modal__payButton'>
+                Pagar
+              </button>
+            </form>
           </ModalPayment>
         )
       }
@@ -51,7 +75,9 @@ export function Home(props) {
         users.map((user) => {
           return (
             <UserList
-              onClick={handleButtonOnClick}
+              onClick={() => {
+                handleButtonOnClick(user);
+              }}
               key={user.id}
               userImg={user.img}
               name={user.name}
@@ -61,6 +87,35 @@ export function Home(props) {
           );
         })
       }
+      {
+        //Modal de pagamento realizado com sucesso
+        false && (
+          <ModalPayment
+            firstSpan='Recibo de pagamento'
+            closeModal={() => setIsCardsModalOpen(false)}>
+            <span className='modal__paymentSucced'>O pagamento foi concluido com sucesso!</span>
+          </ModalPayment>
+        )
+      }
+
+      {
+        //Modal de pagamento realizado sem sucesso
+        false && (
+          <ModalPayment
+            firstSpan='Recibo de pagamento'
+            closeModal={() => setIsCardsModalOpen(false)}>
+            <span className='modal__paymentSucced'>
+              O pagamento <strong>não</strong> foi concluido com sucesso!
+            </span>
+          </ModalPayment>
+        )
+      }
     </>
   );
 }
+
+//Função para verificação do pagamento
+function paymentVerification() {}
+
+//Função para cerificação de valor do pagamento
+function checkValue() {}
---
+++ src/utils/currencyMask.js
@@ -0,0 +1,35 @@
+//Máscara simples
+export function currencyMask(e) {
+  var value = e.target.value;
+  e.target.value = formatCurrency(value);
+}
+
+//Formatando moeda
+function formatCurrency(value) {
+  var valueToFormat = String(value)
+    .replace(/[a-zA-Z\$\., ]/g, "")
+    .replace(/^(0*)/g, "");
+  valueToFormat = valueToFormat.split("").reverse();
+
+  if (valueToFormat.length < 3) {
+    var length = valueToFormat.length;
+    while (length < 3) {
+      valueToFormat.push("0");
+      length = valueToFormat.length;
+    }
+  }
+
+  valueToFormat[2] = valueToFormat[2] + ",";
+  var arrayPoint = valueToFormat.reverse().join("").split(",");
+  arrayPoint[0] = arrayPoint[0].split("").reverse();
+
+  for (let index = 2; index < arrayPoint[0].length; index++) {
+    if (index % 3 == 0) {
+      arrayPoint[0][index] = arrayPoint[0][index] + ".";
+    }
+  }
+  arrayPoint[0] = arrayPoint[0].reverse().join("");
+  arrayPoint = arrayPoint.join(",");
+
+  return "R$ " + arrayPoint;
+}
